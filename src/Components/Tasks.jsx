bee748a23c450c34e27deb92d7f8efc576630cf3
Samina-tenso <tensammi@gmail.com>
2022-02-21 15:44:00 +0100

    fixed strike through

--- src/Components/Tasks.jsx
+++ src/Components/Tasks.jsx
@@ -3,10 +3,14 @@ import React from "react"
 export default function Task({ task, index, completeTask, removeTask }) {
     return (
         <div className="task"
-            style={{ textDecoration: task.completed ? "line-through" : "" }}>
-            {task.title}
+        >
+
+
+            <div style={{ textDecoration: task.completed ? "line-through" : "" }}>
+                {task.title}
+            </div>
             <button className="remove" onClick={() => removeTask(index)}>x</button>
-            <button onClick={() => completeTask(index)}>Completed</button>
+            <button className="completed" onClick={() => completeTask(index)}>Completed</button>
         </div>
     );
 }
